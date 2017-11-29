1d3ecb56186e0750eae205e2357fb24f68718d55
Adam Rowe <adam1rowe1@gmail.com>
2017-11-29 12:52:15 +0000

    db connect

--- php/connect.php
+++ php/connect.php
@@ -3,12 +3,12 @@
 $servername = "192.168.0.2";
 $username = "marlyn";
 $password = "Iz5lHo4g";
-$port = "3306";
+//$port = "3306";
 $dbname = "gopagoda";
 
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $port, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 
 // Check connection
